2ceaa9052f478dd63e2d3162d3c1925eba146483
ZhengyangXu <zhengyang_xu@sina.cn>
2020-06-24 17:24:46 +0800

    "data input"

--- stock_input/stock_precit_input.sql
+++ stock_input/stock_precit_input.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `app.app_forecast_input`(
+CREATE TABLE `stock.stock_predict_input`(
 `business_date` string COMMENT '营业日期',
 `shop_number` string COMMENT '门店编号',
 `item_number` string COMMENT '商品编码',
@@ -9,10 +9,11 @@ CREATE TABLE `app.app_forecast_input`(
 `second_category_name` string COMMENT '中分类名称',
 `first_category_number` string COMMENT '大分类编号',
 `first_category_name` string COMMENT '大分类名称',
-`sale_uom` string COMMENT '销售单位',
 `purchase_qty` double COMMENT '订购数',
 `receive_qty` double COMMENT '进货数',
-`purchase_uom` string COMMENT '进货单位',
+`bf13_sale_qty` double COMMENT '13点前销售数'
+`bf13_cust_num` double COMMENT '13点前客数',
+`bf19_cust_num` double COMMENT '19点前客数',
 `discount_amount` double COMMENT '折扣额',
 `discount_rate` double COMMENT '折扣率',
 `loss_rate` double COMMENT '损耗率',
@@ -38,6 +39,7 @@ CREATE TABLE `app.app_forecast_input`(
 `after_discount_qty` double COMMENT '折扣后数量',
 `before_discount_amount` double COMMENT '折扣前金额',
 `after_discount_amount` double COMMENT '折扣后金额',
+`burst_flag` string COMMNET '是否爆款'
 `province` string COMMENT '省',
 `city` string COMMENT '城市',
 `city_area` string COMMENT '区',
